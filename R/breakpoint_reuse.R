993cccc880efac7e6acb7697c0314eda61838a02
kgori <kevin.c.gori@gmail.com>
2020-05-19 11:29:50 +0100

    Update segmatch function to work with new column names

--- R/breakpoint_reuse.R
+++ R/breakpoint_reuse.R
@@ -3,10 +3,11 @@
 make_segmatch <- function(coordinates, ignore_direction = TRUE) {
 
     # Check the data has the expected columns
-    expected_columns <- c("Order", "chr", "start", "end", "Type", "New.CNV_ID_ext", "Category")
-    stopifnot(
-        intersect(expected_columns, colnames(coordinates)) == expected_columns
-    )
+    expected_columns <- c("Order", "chr", "start", "end", "Type", "CNV_ID_ext", "Category")
+    if (!all(expected_columns %in% colnames(coordinates))) {
+        missing_columns <- paste(setdiff(expected_columns, colnames(coordinates)))
+        stop(paste("Input data is missing the following necessary columns:", missing_columns))
+    }
 
     # "by_columns" = The columns to use as data.table's "by" argument - We will compress the data down to each unique combination of these columns
     by_columns <- if (ignore_direction) {
@@ -18,7 +19,7 @@ make_segmatch <- function(coordinates, ignore_direction = TRUE) {
     # This selection finds all CNVs that share a start pos (BUT not end pos - a separate segments search will account for these),
     # collapses CNVIDs and tumour types into lists, and filters out any matches that occur only within a tumour type
     shared_starts <-
-        unique(coordinates[, .(commonCnvIds = paste0(sort(unique(New.CNV_ID_ext)), collapse=","),
+        unique(coordinates[, .(commonCnvIds = paste0(sort(unique(CNV_ID_ext)), collapse=","),
                                tumourTypes = paste0(sort(unique(Category)), collapse=","),
                                nTumourTypes = uniqueN(Category),
                                Order = min(Order),
@@ -32,7 +33,7 @@ make_segmatch <- function(coordinates, ignore_direction = TRUE) {
     # This selection is as above, but for CNVs that match their end position only.
     by_columns[by_columns == "start"] <- "end"
     shared_ends <-
-        unique(coordinates[, .(commonCnvIds = paste0(sort(unique(New.CNV_ID_ext)), collapse=","),
+        unique(coordinates[, .(commonCnvIds = paste0(sort(unique(CNV_ID_ext)), collapse=","),
                                tumourTypes = paste0(sort(unique(Category)), collapse=","),
                                nTumourTypes = uniqueN(Category),
                                Order = min(Order),
@@ -45,14 +46,14 @@ make_segmatch <- function(coordinates, ignore_direction = TRUE) {
     # This selection finds CNVs that exactly match in both start and end positions, among different tumour types
     by_columns <- c(by_columns[1], "start", by_columns[2:length(by_columns)])
     shared_segments <-
-        unique(coordinates[, .(commonCnvIds = paste0(sort(unique(New.CNV_ID_ext)), collapse=","),
+        unique(coordinates[, .(commonCnvIds = paste0(sort(unique(CNV_ID_ext)), collapse=","),
                                tumourTypes = paste0(sort(unique(Category)), collapse=","),
                                nTumourTypes = uniqueN(Category),
                                dft1Count = sum(Category=="DFT1"),
                                dft2Count = sum(Category=="DFT2"),
                                nonDftdCount = sum(startsWith(Category, "NonDFTD")),
                                Order = min(Order),
-                               nCNVs = uniqueN(New.CNV_ID_ext)),
+                               nCNVs = uniqueN(CNV_ID_ext)),
                            by = by_columns][nCNVs > 1],
                by = by_columns)
     shared_segments[, nCNVs := NULL]
@@ -65,12 +66,12 @@ make_segmatch <- function(coordinates, ignore_direction = TRUE) {
         c("chr", "Type", start="end")
     }
     start_matches_end <- coordinates[coordinates, , on = on_columns, nomatch=0L]
-    start_matches_end <- start_matches_end[, .(commonCnvIds = paste0(sort(unique(c(New.CNV_ID_ext, i.New.CNV_ID_ext))), collapse=","),
+    start_matches_end <- start_matches_end[, .(commonCnvIds = paste0(sort(unique(c(CNV_ID_ext, i.CNV_ID_ext))), collapse=","),
                                                tumourTypes = paste0(sort(unique(c(Category, i.Category))), collapse=","),
                                                nTumourTypes = uniqueN(c(Category, i.Category)),
-                                               dft1Count = unique(rbindlist(list(data.table(New.CNV_ID_ext, Category), data.table(i.New.CNV_ID_ext, i.Category)), use.names=FALSE), by = "New.CNV_ID_ext")[, sum(Category=="DFT1")],
-                                               dft2Count = unique(rbindlist(list(data.table(New.CNV_ID_ext, Category), data.table(i.New.CNV_ID_ext, i.Category)), use.names=FALSE), by = "New.CNV_ID_ext")[, sum(Category=="DFT2")],
-                                               nonDftdCount = unique(rbindlist(list(data.table(New.CNV_ID_ext, Category), data.table(i.New.CNV_ID_ext, i.Category)), use.names=FALSE), by = "New.CNV_ID_ext")[, sum(startsWith(Category, "NonDFTD"))],
+                                               dft1Count = unique(rbindlist(list(data.table(CNV_ID_ext, Category), data.table(i.CNV_ID_ext, i.Category)), use.names=FALSE), by = "CNV_ID_ext")[, sum(Category=="DFT1")],
+                                               dft2Count = unique(rbindlist(list(data.table(CNV_ID_ext, Category), data.table(i.CNV_ID_ext, i.Category)), use.names=FALSE), by = "CNV_ID_ext")[, sum(Category=="DFT2")],
+                                               nonDftdCount = unique(rbindlist(list(data.table(CNV_ID_ext, Category), data.table(i.CNV_ID_ext, i.Category)), use.names=FALSE), by = "CNV_ID_ext")[, sum(startsWith(Category, "NonDFTD"))],
                                                Order = min(c(Order, i.Order))), by = by_columns]
 
     # Merge lists
